8ca50b456c7a0e79f2638c87f7ecda389023af6c
Adnan Qaizar <adnanqaizar@gmail.com>
2018-11-27 18:31:14 +0500

    Updates

---
+++ precache-manifest.91e0d2b8eb936a83dbee0a497cc931fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f20e2c7e11feac878f85",
+    "url": "/bookstore-app/static/js/runtime~main.f20e2c7e.js"
+  },
+  {
+    "revision": "bd701719d734d7a5a102",
+    "url": "/bookstore-app/static/js/main.bd701719.chunk.js"
+  },
+  {
+    "revision": "c145dbf2a3ebc0e18379",
+    "url": "/bookstore-app/static/js/1.c145dbf2.chunk.js"
+  },
+  {
+    "revision": "bd701719d734d7a5a102",
+    "url": "/bookstore-app/static/css/main.eae18ba3.chunk.css"
+  },
+  {
+    "revision": "b4ccff2baec466c95c8ea13a2acd0cba",
+    "url": "/bookstore-app/index.html"
+  }
+];
